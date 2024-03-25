fc573f7b377b29cee9764e58c277887f06aa843c
chankanyin <kanyin.cky@gmail.com>
2024-03-26 00:37:36 +0800

    Model completed

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -12,7 +12,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["👻","🎃","🕷️","😈","💀","🕸️","🙀","👹","🦊","🐰","🐭","🐶"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        return MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
             if emojis.indices.contains(pairIndex) {
                 return emojis[pairIndex]
             } else {
